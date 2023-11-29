c4815d68ee519433ce3044016663e886787e3931
De Oka <115537848+DeOkanee@users.noreply.github.com>
2023-11-29 10:22:24 +0800

    new

---
+++ assets/js/RAGU.js
@@ -0,0 +1,11 @@
+const ragu = document.getElementById("ragu");
+				ragu.addEventListener("click", function () {
+				Swal.fire({
+                    icon: "question",
+					text: "Jika yang anda pilih belum anda ketahui, periksa kembali apa yang anda lihat,\n Karena apa yang anda pilih tergantung dari apa yang anda lihat.",
+                    footer: '<a href="index.html">TENTUKAN PILIHANMU !</a>'
+                });
+				});
+
+
+             
